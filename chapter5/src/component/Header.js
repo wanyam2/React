218f420eca4ff4162a4dad0d6490aa709b1a3599
EUNCHAE <2snha@naver.com>
2024-01-03 01:45:27 +0900

    new

---
+++ chapter5/src/component/Header.js
@@ -0,0 +1,8 @@
+function Header() { 
+    return (
+    <header>
+        <h1>header</h1>
+    </header>
+    );
+}
+export default Header; // Header 컴포넌트를 다른 파일에서 사용할 수 있도록 내보냄. 이때 원하는 이름으로 불러올 수 있도록 모듈의 기본값으로 내보냄
---
+++ chapter5/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />); // 리액트의 루트 요소 아래에 App 컴포넌트를 배치해 렌더링
